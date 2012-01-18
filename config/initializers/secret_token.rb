51a08ac90f514a374f88071c6a6cd699cda52baa
Grzegorz Świrski <sognat@gmail.com>
2012-01-18 13:22:53 +0100

    Imported rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Morse::Application.config.secret_token = '144175068bbbe66b6e5ec762784ec583f1cf17723c2a2db28d40e7030673681b726c52e295436938523bf7dd5d763bad4edeb6c3294b992555424183d60ed301'
