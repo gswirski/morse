9374d95957ddef3bee5223d725347fa7dc57db1f
Grzegorz Świrski <sognat@gmail.com>
2012-01-23 17:07:44 +0100

    Added basic tests

--- test/factories/pastes.rb
+++ test/factories/pastes.rb
@@ -2,5 +2,29 @@
 
 FactoryGirl.define do
   factory :paste do
+    code <<-eos
+      #include <cstdio>
+      using namespace std;
+
+      // nice comment
+      int main() {
+        return 0;
+      }
+    eos
+
+    sequence(:slug) { |n| "paste_slug_#{n}" }
+  end
+
+  factory :named_paste, :parent => :paste do
+    name "solution.cpp"
+  end
+  
+  factory :syntax_paste, :parent => :paste do
+    syntax "cpp"
+  end
+  
+  factory :full_paste, :parent => :paste do
+    name "solution.php" # funny name...
+    syntax "cpp"
   end
 end
