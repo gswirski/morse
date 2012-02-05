cb07ed01c914b274a36b026fe57ffa67cbb22f61
Grzegorz Świrski <sognat@gmail.com>
2012-02-05 11:43:47 +0100

    Use plain text when pygmentize exit status != 0

--- app/models/paste.rb
+++ app/models/paste.rb
@@ -69,6 +69,12 @@ class Paste < ActiveRecord::Base
 
       io.read
     end
+
+    if $?.exitstatus != 0
+      self.syntax = "txt"
+      colorize
+    end
+
     self.highlighted_at = Time.now
   end
 end
