0c81f21116c62ff2bf028821a6a660e04d1cfe26
Grzegorz Świrski <sognat@gmail.com>
2012-02-26 14:31:26 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Morse::Application.config.secret_token = '139d5d8af6131a1fa06bf0b09e31119d8dc0d205a072ac47630147350098ba501e43addcb62e56fdb33b90dbdffdfe8143f2017b60657eb5853f50b87a3578d1'
