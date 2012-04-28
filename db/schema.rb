15f34ac66b4799d87635af835a48fcfbf7d33fa4
Grzegorz Świrski <sognat@gmail.com>
2012-04-28 14:39:34 +0200

    synchronized schema.rb with heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,30 +11,43 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120428110934) do
+ActiveRecord::Schema.define(:version => 20120428112104) do
 
   create_table "pastes", :force => true do |t|
     t.string   "name"
     t.string   "slug"
     t.string   "syntax"
     t.text     "code"
+    t.integer  "user_id"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
+    t.text     "highlighted"
+    t.datetime "highlighted_at"
     t.text     "highlighted_cache"
-    t.integer  "user_id"
     t.string   "month"
   end
 
-  add_index "pastes", ["user_id"], :name => "index_pastes_on_user_id"
-
   create_table "users", :force => true do |t|
-    t.string   "username"
-    t.string   "password_digest"
-    t.datetime "created_at",           :null => false
-    t.datetime "updated_at",           :null => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.string   "authentication_token"
+    t.datetime "created_at",                                               :null => false
+    t.datetime "updated_at",                                               :null => false
+    t.string   "username"
+    t.boolean  "admin",                                 :default => false
+    t.string   "password_digest"
   end
 
+  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
